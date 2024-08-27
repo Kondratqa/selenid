f529858bff096027f00fe828b67b732c3e121edf
Kinomagnum <kinomagnum@gmail.com>
2024-08-27 11:38:39 +0300

    initialCommitSelenide

--- src/test/java/ru/netology/test/CallBackDataFormSelenide.java
+++ src/test/java/ru/netology/test/CallBackDataFormSelenide.java
@@ -1,17 +1,20 @@
 package ru.netology.test;
+import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class CallBackDataForm {
+public class CallBackDataFormSelenide {
     private WebDriver driver;
     @BeforeAll
     public static void setupAll() {
@@ -33,21 +36,19 @@ public class CallBackDataForm {
     }
     @Test
     void shouldTestCallback() throws InterruptedException {
-        driver.get("http://localhost:9999");
+        open("http://localhost:9999");
 
-        driver.findElement(By.cssSelector("[data-test-id=city] input")).sendKeys("Москва");
-        driver.findElement(By.cssSelector("[data-test-id=date] input")).sendKeys("28.05.2023");
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79281234567");
-
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.cssSelector("span[class=button__text]")).click();
+        $$(".input__control").findBy(attribute("placeholder", "Город")).sendKeys("Москва");
+        $$(".input__control").findBy(attribute("pattern", "[0-9.]*")).sendKeys("28.05.2023");
+        $$(".input__control").findBy(attribute("name", "name")).sendKeys("Иванов Иван");
+        $$(".input__control").findBy(attribute("name", "phone")).sendKeys("+79281234567");
+        $(".checkbox__box").click();
+        $("span[class=button__text]").click();
         Thread.sleep(10000);
+        SelenideElement successMessageElement = $("[data-test-id=notification]");
+        successMessageElement.shouldBe(visible);
 
-        WebElement successMessageElement = driver.findElement(By.cssSelector("[data-test-id=notification]"));
-        assertTrue(successMessageElement.isDisplayed(), "Сообщение об успешной отправке должно быть видимым.");
-
-        String text = driver.findElement(By.cssSelector("[data-test-id=notification]")).getText();
+        String text = successMessageElement.getText();
         System.out.println(text);
     }
 
