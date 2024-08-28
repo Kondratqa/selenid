42bc5c0567f95f2f7962025560702122a4da656f
Kinomagnum <kinomagnum@gmail.com>
2024-08-28 21:20:55 +0300

    initialCommitPushFinal

--- src/test/java/ru/netology/test/CallBackDataFormSelenide.java
+++ src/test/java/ru/netology/test/CallBackDataFormSelenide.java
@@ -1,23 +1,36 @@
 package ru.netology.test;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Condition.visible;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CallBackDataFormSelenide {
-    @Test
-    void shouldTestCallback() throws InterruptedException {
-        open("http://localhost:9999");
+        @Test
+        void shouldTestCallback() {
+            open("http://localhost:9999");
 
-        $("[data-test-id=city] input").sendKeys("Москва");
-        $("[data-test-id=date] input").sendKeys("28.05.2025");
-        $("[data-test-id=name] input").sendKeys("Москва");
-        $("[data-test-id=phone] input").sendKeys("+79000000000");
-        $(".checkbox__box").click();
-        $("span[class=button__text]").click();
-        $("[data-test-id=notification]");
+            $("[data-test-id=city] input").sendKeys("Москва");
 
-    }
+            LocalDate localDate = LocalDate.now().plusDays(10);
+            String date = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(localDate);
 
+            $("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a");
+            $("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
+            $("[data-test-id=date] input").sendKeys( date);
+            $("[data-test-id=name] input").sendKeys("Москва");
+            $("[data-test-id=phone] input").sendKeys("+79000000000");
+            $(".checkbox__box").click();
+            $("span[class=button__text]").click();
+
+            SelenideElement successMessageElement = $("[data-test-id=notification]");
+            successMessageElement.shouldBe(Condition.visible, Duration.ofSeconds(20));
+            successMessageElement.shouldHave(text("Встреча успешно забронирована на " + date));
+        }
 }
